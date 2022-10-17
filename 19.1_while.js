d4d1ff05cbedafa4f13546ca86f38abd32e5bd82
Muhammmadandri <prajawiratama08@gmail.com>
2022-10-17 14:57:14 +0700

    17-10-2022_commit file While,for,do while_andri

---
+++ 19.1_while.js
@@ -0,0 +1,54 @@
+//
+var i = 1
+var j = 4
+var k = 1
+    // perulangan for
+for (; i <= 5;) {
+    console.log(i)
+    i += 1
+}
+// perulangan while 
+while (j <= 5) {
+    console.log(j)
+    j += 1
+}
+// perulangan do while 
+do {
+    console.log(k)
+    k += 1
+} while (k <= 5);
+
+//perulangan Break
+for (i = 0; i <= 10; i++) {
+    if (i == 5) {
+        break;
+    }
+    console.log(i);
+}
+
+//perulangan continue
+for (i = 0; i <= 10; i++) {
+    if (i == 5) {
+        continue;
+    }
+    console.log(i);
+}
+
+//tugas
+console.log("Room number");
+for (x = 0; x <= 15; x++) {
+    if (i == 13) {
+        continue;
+    }
+    console.log(x);
+}
+
+//perulangan SUM
+console.log("perulangan SUM")
+var sum = 0
+for (i = 4; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
